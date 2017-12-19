89de6800bfccc35a2b5da7b8baee2458de0f8ecd
yueqi1125 <yueqi1125@qq.com>
2017-12-19 18:28:22 +0800

    Signed-off-by: yueqi1125 <yueqi1125@qq.com>

--- src/main/java/com/xt/java/user/model/Student.java
+++ src/main/java/com/xt/java/user/model/Student.java
@@ -2,4 +2,53 @@ package com.xt.java.user.model;
 
 public class Student {
 
+	private String stuNo;
+	
+	private String stuName;
+	
+	private String college;
+	
+	private String tel;
+	
+	private int age;
+
+	public String getStuNo() {
+		return stuNo;
+	}
+
+	public void setStuNo(String stuNo) {
+		this.stuNo = stuNo;
+	}
+
+	public String getStuName() {
+		return stuName;
+	}
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+
+	public String getCollege() {
+		return college;
+	}
+
+	public void setCollege(String college) {
+		this.college = college;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
--- src/main/java/com/xt/java/user/model/User.java
+++ src/main/java/com/xt/java/user/model/User.java
@@ -2,4 +2,24 @@ package com.xt.java.user.model;
 
 public class User {
 
+	private String userName;
+	
+	private String passwd;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
 }
